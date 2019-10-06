6c63a0ef71f4784fa5133b545f77b89448a7d543
Kendrick Tan <kendricktan0814@gmail.com>
2019-10-06 17:13:00 +0900

    get merkle tree

--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -13,8 +13,21 @@ const flipBits = (x: Array<Number>): Array<Number> => {
 const _stringifyBigInts = (x: BigInt): String => stringifyBigInts(x)
 const _unstringifyBigInts = (x: Any): BigInt => unstringifyBigInts(x)
 
+const getLatestDeployedAddress = (contractJson: Object): String => {
+  const { networks } = contractJson
+
+  if (networks === undefined || networks.length === 0) {
+    throw new Error('Contract not deployed yet!')
+  }
+
+  const latestTimestamp = Object.keys(networks).sort((a, b) => b - a)[0]
+
+  return networks[latestTimestamp].address
+}
+
 module.exports = {
   flipBits,
+  getLatestDeployedAddress,
   stringifyBigInts: _stringifyBigInts,
   unstringifyBigInts: _unstringifyBigInts
 }
--- test/testCoordinatorLogic.js
+++ test/testCoordinatorLogic.js
@@ -1,11 +1,57 @@
 
 const supertest = require('supertest')
+const ethers = require('ethers')
+
+const { unstringifyBigInts } = require('../_build/utils/helpers')
+const { randomPrivateKey, privateToPublicKey, decrypt } = require('../_build/utils/crypto')
 const { app } = require('../_build/index')
+const { getLatestDeployedAddress } = require('../_build/utils/helpers')
+
+// Settings up provider and contract instance
+const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545')
+const privateKey = '0x989d5b4da447ba1c7f5d48e3b4310d0eec08d4abd0f126b58249598abd8f4c37'
+const wallet = new ethers.Wallet(privateKey, provider)
+
+const maciContractDef = require('../_build/contracts/MACI.json')
+const maciContract = new ethers.Contract(
+  getLatestDeployedAddress(maciContractDef),
+  maciContractDef.abi,
+  wallet
+)
+
+const merkleTreeContractDef = require('../_build/contracts/MerkleTree.json')
+const merkleTreeNetworkTimestamps = Object.keys(merkleTreeContractDef.networks)
+  .sort((a, b) => b-a)
+const executionStateMT = new ethers.Contract(
+  merkleTreeContractDef.networks[merkleTreeNetworkTimestamps[1]].address,
+  merkleTreeContractDef.abi,
+  wallet
+)
+const registryMT = new ethers.Contract(
+  merkleTreeContractDef.networks[merkleTreeNetworkTimestamps[0]].address,
+  merkleTreeContractDef.abi,
+  wallet
+)
 
 describe('GET /', function () {
-  it('/ should return 404', () => {
+  let coordinatorPublicKey
+
+  const userPrvKey = randomPrivateKey()
+  const userPubKey = privateToPublicKey(userPrvKey)
+
+  before('Setup Contract for coordinator test', async () => {
+    const resp = await supertest(app).get('/publickey')
+    const { publicKey } = resp.body
+    coordinatorPublicKey = unstringifyBigInts(publicKey)
+  })
+
+  it('/ should return 404', async () => {
     supertest(app)
       .get('/')
       .expect(404)
+  })
+
+  it('testing coordinator logic', async () => {
+    // const merkleRootResp = 
   })
 })
