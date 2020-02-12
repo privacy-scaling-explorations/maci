d5b9564b5b30423ad33f78d74b3ea03b10830443
Koh Wei Jie <contact@kohweijie.com>
2020-02-12 20:04:12 +0800

    fixed bug in checksumSnarks.sh

--- circuits/scripts/checksumSnarks.sh
+++ circuits/scripts/checksumSnarks.sh
@@ -7,6 +7,6 @@ cd "$(dirname "$0")"
 mkdir -p ../build
 cd ../build
 
-find ../circom -not -path "../circom/test/*" -type f -exec md5sum {} \; | sort -k 2 | md5sum
+find ../circom -not -path "../circom/test/*" -type f -exec md5sum {} \; | sort -k 2 | md5sum > .snark_checksum
 echo 'snark checksum:'
 cat .snark_checksum
