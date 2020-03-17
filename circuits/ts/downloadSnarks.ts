2ad91deaafc6bb517fc93edb23a05f83e0571bd2
Koh Wei Jie <contact@kohweijie.com>
2020-03-17 14:10:54 +0800

    updated dropbox links

--- circuits/ts/downloadSnarks.ts
+++ circuits/ts/downloadSnarks.ts
@@ -5,35 +5,35 @@ const main = async () => {
     const files = [
         {
             name: 'BatchUpdateStateTreeVerifier.sol',
-            url: 'https://www.dropbox.com/s/2zi2dlet7ziqvio/BatchUpdateStateTreeVerifier.sol?dl=1',
+            url: 'https://www.dropbox.com/s/sxzoz4di2qvmw7j/BatchUpdateStateTreeVerifier.sol?dl=1',
         },
         {
             name: 'batchUstCircuit.json',
-            url: 'https://www.dropbox.com/s/kglrkfts17t5ub9/batchUstCircuit.json?dl=1',
+            url: 'https://www.dropbox.com/s/0ssyiq83dk3d8lc/batchUstCircuit.json?dl=1',
         },
         {
             name: 'batchUstPk.bin',
-            url: 'https://www.dropbox.com/s/1d1urt75uwl32mu/batchUstPk.bin?dl=1',
+            url: 'https://www.dropbox.com/s/typemzqabjo195q/batchUstPk.bin?dl=1',
         },
         {
             name: 'batchUstVk.json',
-            url: 'https://www.dropbox.com/s/7hjb3x1w0w7udnn/batchUstVk.json?dl=1',
+            url: 'https://www.dropbox.com/s/khsnl7ixtw5ksym/batchUstVk.json?dl=1',
         },
         {
             name: 'QuadVoteTallyVerifier.sol',
-            url: 'https://www.dropbox.com/s/sb1qz788ni7prdg/QuadVoteTallyVerifier.sol?dl=1',
+            url: 'https://www.dropbox.com/s/7apbpbwgfgtxfnc/QuadVoteTallyVerifier.sol?dl=1',
         },
         {
             name: 'qvtCircuit.json',
-            url: 'https://www.dropbox.com/s/a619ih90hozhzk0/qvtCircuit.json?dl=1',
+            url: 'https://www.dropbox.com/s/nucj8dat4udlpb0/qvtCircuit.json?dl=1',
         },
         {
             name: 'qvtPk.bin',
-            url: 'https://www.dropbox.com/s/jh98gtzvq9rlaeu/qvtPk.bin?dl=1',
+            url: 'https://www.dropbox.com/s/sgbssqxviiuu9yq/qvtPk.bin?dl=1',
         },
         {
             name: 'qvtVk.json',
-            url: 'https://www.dropbox.com/s/5oksb932vocleif/qvtVk.json?dl=1',
+            url: 'https://www.dropbox.com/s/qvk4hc4lpezwvgl/qvtVk.json?dl=1',
         },
     ]
 
