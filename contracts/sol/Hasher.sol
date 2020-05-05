5fc5a0a35129b9aa152937209acb43839e23efcd
Chih Cheng Liang <chihchengliang@gmail.com>
2020-05-05 16:59:54 +0800

    implement hashers

--- contracts/sol/Hasher.sol
+++ contracts/sol/Hasher.sol
@@ -7,45 +7,53 @@
 
 pragma solidity ^0.5.0;
 
-import { MiMC } from './MiMC.sol';
-import { SnarkConstants } from './SnarkConstants.sol';
+import {PoseidonT3, PoseidonT6} from "./Poseidon.sol";
+
+import {SnarkConstants} from "./SnarkConstants.sol";
+
 
 contract Hasher is SnarkConstants {
-    function hash(uint256[] memory array) public pure returns (uint256) {
-        uint256 R = 0;
-        uint256 C = 0;
-
-        for (uint256 i = 0; i < array.length; i++) {
-            R = addmod(R, array[i], SNARK_SCALAR_FIELD);
-            (R, C) = MiMC.MiMCSponge(R, C);
-        }
-        
-        return R;
+    function hash5(uint256[] memory array) public pure returns (uint256) {
+        return PoseidonT6.poseidon(array);
     }
 
-    /*
-     * Concatenates and hashes two `uint256` values (left and right) using
-     * a combination of MiMCSponge and `addmod`.
-     * @param _left The first value
-     * @param _right The second value
-     * @return The uint256 hash of _left and _right
-     */
-    function hashLeftRight(uint256 _left, uint256 _right) public pure returns (uint256) {
+    function hash11(uint256[] memory array) public pure returns (uint256) {
+        if (array.length < 11) {
+            for (i = array.length; i < 11; i++) {
+                array.push(0);
+            }
+        }
+        return
+            PoseidonT3.poseidon(
+                [
+                    PoseidonT3.poseidon(
+                        [
+                            PoseidonT6.poseidon(
+                                array[0],
+                                array[1],
+                                array[2],
+                                array[3],
+                                array[4]
+                            ),
+                            PoseidonT6.poseidon(
+                                array[5],
+                                array[6],
+                                array[7],
+                                array[8],
+                                array[9]
+                            )
+                        ]
+                    ),
+                    array[10]
+                ]
+            );
+    }
 
-        // Solidity documentation states:
-        // `addmod(uint x, uint y, uint k) returns (uint)`:
-        // compute (x + y) % k where the addition is performed with arbitrary
-        // precision and does not wrap around at 2**256. Assert that k != 0
-        // starting from version 0.5.0.
-
-        uint256 R = _left;
-        uint256 C = 0;
-
-        (R, C) = MiMC.MiMCSponge(R, 0);
-
-        R = addmod(R, _right, SNARK_SCALAR_FIELD);
-        (R, C) = MiMC.MiMCSponge(R, C);
-
-        return R;
+    function hashLeftRight(uint256 _left, uint256 _right)
+        public
+        pure
+        returns (uint256)
+    {
+        return PoseidonT3.poseidon([_left, _right]);
     }
 }
