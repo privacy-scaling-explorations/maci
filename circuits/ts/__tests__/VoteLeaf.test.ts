a32728bbd3001a6c4d9244533c0c5940a9bcee85
Koh Wei Jie <contact@kohweijie.com>
2020-06-12 17:56:40 +0800

    bugfix: no-op if the vote leaf range check fails

--- circuits/ts/__tests__/VoteLeaf.test.ts
+++ circuits/ts/__tests__/VoteLeaf.test.ts
@@ -48,4 +48,21 @@ describe('Vote leaf circuits', () => {
         const outputOut = witness[circuit.getSignalIdx('main.out')].toString()
         expect(outputOut).toEqual('0')
     })
+
+    it('correctly calculates the square of voice credits spent in a vote leaf', async () => {
+        circuit = await compileAndLoadCircuit('test/calculateSquaredVoteLeaf_test.circom')
+        const pos = bigInt(1234)
+        const neg = bigInt(4567)
+        const voteLeaf = new VoteLeaf(pos, neg)
+
+        const circuitInputs = {
+            packedLeaf: voteLeaf.pack(),
+        }
+
+        const witness = circuit.calculateWitness(circuitInputs)
+        expect(circuit.checkWitness(witness)).toBeTruthy()
+
+        const output = witness[circuit.getSignalIdx('main.squared')].toString()
+        expect(output).toEqual((pos + neg).pow(bigInt(2)).toString())
+    })
 })
