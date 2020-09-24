cab3a8075ee99cf77aaef0365f970fa39d562b96
Koh Wei Jie <contact@kohweijie.com>
2020-09-24 09:29:45 +0800

    return to npm OZ; download solc automatically

--- contracts/scripts/compileSol.sh
+++ contracts/scripts/compileSol.sh
@@ -3,21 +3,26 @@ set -e
 
 cd "$(dirname "$0")"
 cd ..
-echo 'Building contracts'
 
 # Delete old files
 rm -rf ./compiled/*
 
-if [[ -z "${SOLC_PATH}" ]]; then
-    # Assumes that you have solc 0.5.17 (https://github.com/ethereum/solidity/releases/tag/v0.5.17) installed in your PATH
-    solcBin="solc"
-else
-    # Otherwise, you can specify the path to solc 0.5.17
-    solcBin="${SOLC_PATH}"
-fi
+#if [[ -z "${SOLC_PATH}" ]]; then
+    ## Assumes that you have solc 0.5.17 (https://github.com/ethereum/solidity/releases/tag/v0.5.17) installed in your PATH
+    #solcBin="solc"
+#else
+    ## Otherwise, you can specify the path to solc 0.5.17
+    #solcBin="${SOLC_PATH}"
+#fi
 
-paths="$(pwd)/sol/"
+echo 'Downloading solc...'
+solcBin=$(pwd)/solc
+wget -nc -q -O $solcBin https://github.com/ethereum/solidity/releases/download/v0.5.17/solc-static-linux
+chmod a+x $solcBin
 
+paths="$(pwd)/sol/,$(pwd)/node_modules/openzeppelin-solidity/"
+
+echo 'Building contracts'
 $solcBin -o ./compiled ./sol/*.sol --overwrite --optimize --bin --abi --bin-runtime --allow-paths=$paths
 $solcBin -o ./compiled ./sol/**/*.sol --overwrite --optimize --bin --abi --bin-runtime --allow-paths=$paths
 
--- contracts/sol/IncrementalQuinTree.sol
+++ contracts/sol/IncrementalQuinTree.sol
@@ -23,7 +23,7 @@ pragma solidity ^0.5.0;
 
 import { SnarkConstants } from "./SnarkConstants.sol";
 import { Hasher } from "./Hasher.sol";
-import { Ownable } from "./openzeppelin-solidity//contracts/ownership/Ownable.sol";
+import { Ownable } from "../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 /*
  * An incremental Merkle tree which supports up to 5 leaves per node.
