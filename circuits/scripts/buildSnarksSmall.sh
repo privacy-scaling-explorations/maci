6276405b9769c6be9dc0f568fd78b6d72c9674e2
Koh Wei Jie <contact@kohweijie.com>
2020-09-30 18:32:45 +0800

    enable zkutil

--- circuits/scripts/buildSnarksSmall.sh
+++ circuits/scripts/buildSnarksSmall.sh
@@ -7,10 +7,10 @@ cd ..
 mkdir -p build
 
 echo "Building batchUpdateStateTree_small"
-NODE_OPTIONS=--max-old-space-size=16384 time node --stack-size=16384 build/buildSnarks.js -i circom/prod/batchUpdateStateTree_small.circom -j build/batchUstSmall.r1cs -w build/batchUstSmall.wasm -p build/batchUstPkSmall.json -v build/batchUstVkSmall.json -s build/BatchUpdateStateTreeVerifierSmall.sol -vs BatchUpdateStateTreeVerifierSmall -z build/batchUstSmall.zkey
+NODE_OPTIONS=--max-old-space-size=16384 time node --stack-size=16384 build/buildSnarks.js -i circom/prod/batchUpdateStateTree_small.circom -j build/batchUstSmall.r1cs -w build/batchUstSmall.wasm -p build/batchUstPkSmall.json -v build/batchUstVkSmall.json -s build/BatchUpdateStateTreeVerifierSmall.sol -vs BatchUpdateStateTreeVerifierSmall -pr build/batchUstSmall.params
 
 echo "Building quadVoteTally_small"
-NODE_OPTIONS=--max-old-space-size=16384 time node --stack-size=16384 build/buildSnarks.js -i circom/prod/quadVoteTally_small.circom -j build/qvtCircuitSmall.r1cs -w build/qvtSmall.wasm -p build/qvtPkSmall.bin -v build/qvtVkSmall.json -s build/QuadVoteTallyVerifierSmall.sol -vs QuadVoteTallyVerifierSmall -z build/qvtSmall.zkey
+NODE_OPTIONS=--max-old-space-size=16384 time node --stack-size=16384 build/buildSnarks.js -i circom/prod/quadVoteTally_small.circom -j build/qvtCircuitSmall.r1cs -w build/qvtSmall.wasm -p build/qvtPkSmall.bin -v build/qvtVkSmall.json -s build/QuadVoteTallyVerifierSmall.sol -vs QuadVoteTallyVerifierSmall -pr build/qvtSmall.params
 
 echo 'Copying BatchUpdateStateTreeVerifier.sol to contracts/sol.'
 cp ./build/BatchUpdateStateTreeVerifierSmall.sol ../contracts/sol/
--- circuits/ts/genVerifier.ts
+++ circuits/ts/genVerifier.ts
@@ -14,8 +14,8 @@ const genSnarkVerifierSol = (
 
     template = template.replace('<%contract_name%>', contractName)
 
-    const vkalpha1 = `uint256(${vk.vk_alpha_1[0].toString()}),`+
-                     `uint256(${vk.vk_alpha_1[1].toString()})`
+    const vkalpha1 = `uint256(${vk.vk_alfa_1[0].toString()}),`+
+                     `uint256(${vk.vk_alfa_1[1].toString()})`
     template = template.replace('<%vk_alpha1%>', vkalpha1)
 
     const vkbeta2 = `[uint256(${vk.vk_beta_2[0][1].toString()}),`+
