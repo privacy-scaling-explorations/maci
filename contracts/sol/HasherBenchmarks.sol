b87cc8ab247148a55e256c9b813f0a7fde51469a
Koh Wei Jie <contact@kohweijie.com>
2020-11-14 16:20:30 +0000

    changed HasherBenchmarks functions to non-pure

--- contracts/sol/HasherBenchmarks.sol
+++ contracts/sol/HasherBenchmarks.sol
@@ -4,15 +4,17 @@ pragma solidity ^0.6.12;
 import { Hasher } from "./Hasher.sol";
 
 contract HasherBenchmarks is Hasher {
-    function hash5Benchmark(uint256[] memory array) public pure {
+    // These functions are used to measure gas consumption so they shouldn't be
+    // marked as pure
+    function hash5Benchmark(uint256[] memory array) public {
         hash5(array);
     }
 
-    function hash11Benchmark(uint256[] memory array) public pure {
+    function hash11Benchmark(uint256[] memory array) public {
         hash11(array);
     }
 
-    function hashLeftRightBenchmark(uint256 _left, uint256 _right) public pure {
+    function hashLeftRightBenchmark(uint256 _left, uint256 _right) public {
         hashLeftRight(_left, _right);
     }
 }
