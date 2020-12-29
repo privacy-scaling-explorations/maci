72323301df245d29e661fb76b6c0add9ddff8ec9
Koh Wei Jie <contact@kohweijie.com>
2020-12-29 12:36:01 +0000

    compileSol supports macos now (see PR #196); update @openzeppelin/contracts

--- contracts/scripts/compileSol.sh
+++ contracts/scripts/compileSol.sh
@@ -8,8 +8,14 @@ cd ..
 rm -rf ./compiled/*
 
 echo 'Downloading solc...'
+case "$OSTYPE" in
+  darwin*)  solcPlatform="solc-macos" ;;
+  linux*)   solcPlatform="solc-static-linux" ;;
+  *)        solcPlatform="solc-static-linux" ;;
+esac
 solcBin=$(pwd)/solc
-wget -nc -q -O $solcBin https://github.com/ethereum/solidity/releases/download/v0.7.2/solc-static-linux
+wget -nc -q -O $solcBin https://github.com/ethereum/solidity/releases/download/v0.7.6/${solcPlatform}
+
 chmod a+x $solcBin
 
 paths="$(pwd)/sol/,$(pwd)/node_modules/@openzeppelin/"
