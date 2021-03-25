46bb3c66ce9978f0dcf28a1ff4a13a9aa5851392
Cory Dickson <cory.dickson@protonmail.com>
2021-03-24 20:03:19 -0400

    Update travis to test contracts

--- contracts/scripts/runTestsInCircleCi.sh
+++ contracts/scripts/runTestsInCircleCi.sh
@@ -5,9 +5,9 @@ cd ..
 
 npm run hardhat &
 sleep 3 &&
-NODE_OPTIONS=--max-old-space-size=4096 npx jest --force-exit batchProcessMessageAndQuadVoteTally.test.ts &&
-npx jest --force-exit SignUp.test.ts &&
-npx jest --force-exit PublishMessage.test.ts &&
+NODE_OPTIONS=--max-old-space-size=4096
+#npx jest --force-exit batchProcessMessageAndQuadVoteTally.test.ts &&
+#npx jest --force-exit MACI.test.ts &&
+npx jest --force-exit DomainObjs.test.ts &&
 npx jest --force-exit Hasher.test.ts &&
-npx jest --force-exit VerifyTally.test.ts &&
 npx jest --force-exit AccQueue.test.ts
