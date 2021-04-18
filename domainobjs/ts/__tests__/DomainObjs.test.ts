2a0ca2593c50f171513f10c9b264e7ad79312e11
Koh Wei Jie <contact@kohweijie.com>
2021-04-18 12:40:55 +0100

    fixed domainobjs test

--- domainobjs/ts/__tests__/DomainObjs.test.ts
+++ domainobjs/ts/__tests__/DomainObjs.test.ts
@@ -53,20 +53,20 @@ describe('Domain objects', () => {
             expect(d.vk_alpha_1[0]).toEqual(vk.alpha1.x.toString())
             expect(d.vk_alpha_1[1]).toEqual(vk.alpha1.y.toString())
 
-            expect(d.vk_beta_2[0][0]).toEqual(vk.beta2.x[0].toString())
-            expect(d.vk_beta_2[0][1]).toEqual(vk.beta2.x[1].toString())
-            expect(d.vk_beta_2[1][0]).toEqual(vk.beta2.y[0].toString())
-            expect(d.vk_beta_2[1][1]).toEqual(vk.beta2.y[1].toString())
+            expect(d.vk_beta_2[0][0]).toEqual(vk.beta2.x[1].toString())
+            expect(d.vk_beta_2[0][1]).toEqual(vk.beta2.x[0].toString())
+            expect(d.vk_beta_2[1][0]).toEqual(vk.beta2.y[1].toString())
+            expect(d.vk_beta_2[1][1]).toEqual(vk.beta2.y[0].toString())
 
-            expect(d.vk_gamma_2[0][0]).toEqual(vk.gamma2.x[0].toString())
-            expect(d.vk_gamma_2[0][1]).toEqual(vk.gamma2.x[1].toString())
-            expect(d.vk_gamma_2[1][0]).toEqual(vk.gamma2.y[0].toString())
-            expect(d.vk_gamma_2[1][1]).toEqual(vk.gamma2.y[1].toString())
+            expect(d.vk_gamma_2[0][0]).toEqual(vk.gamma2.x[1].toString())
+            expect(d.vk_gamma_2[0][1]).toEqual(vk.gamma2.x[0].toString())
+            expect(d.vk_gamma_2[1][0]).toEqual(vk.gamma2.y[1].toString())
+            expect(d.vk_gamma_2[1][1]).toEqual(vk.gamma2.y[0].toString())
 
-            expect(d.vk_delta_2[0][0]).toEqual(vk.delta2.x[0].toString())
-            expect(d.vk_delta_2[0][1]).toEqual(vk.delta2.x[1].toString())
-            expect(d.vk_delta_2[1][0]).toEqual(vk.delta2.y[0].toString())
-            expect(d.vk_delta_2[1][1]).toEqual(vk.delta2.y[1].toString())
+            expect(d.vk_delta_2[0][0]).toEqual(vk.delta2.x[1].toString())
+            expect(d.vk_delta_2[0][1]).toEqual(vk.delta2.x[0].toString())
+            expect(d.vk_delta_2[1][0]).toEqual(vk.delta2.y[1].toString())
+            expect(d.vk_delta_2[1][1]).toEqual(vk.delta2.y[0].toString())
 
             expect(d.IC.length).toEqual(vk.ic.length)
             for (let i = 0 ; i < d.IC.length; i ++) {
