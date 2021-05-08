11e189f3bc21a614744ecc30d1d36af63b1cd4ba
Koh Wei Jie <contact@kohweijie.com>
2021-05-08 13:40:29 +0100

    travis sans integration tests

--- integrationTests/scripts/runTestsInCi.sh
+++ integrationTests/scripts/runTestsInCi.sh
@@ -11,11 +11,20 @@ wget --quiet -O ../cli/zkeys/ProcessMessages_10-2-1-2.test.0.zkey \
 wget --quiet -O ../cli/zkeys/ProcessMessages_10-2-1-2.test \
     https://macitestcircuits.blob.core.windows.net/test/ProcessMessages_10-2-1-2.test
 
+wget --quiet -O ../cli/zkeys/ProcessMessages_10-2-1-2.test.dat \
+    https://macitestcircuits.blob.core.windows.net/test/ProcessMessages_10-2-1-2.test.dat
+
 wget --quiet -O ../cli/zkeys/TallyVotes_10-1-2.test.0.zkey \
     https://macitestcircuits.blob.core.windows.net/test/TallyVotes_10-1-2.test.0.zkey
 
 wget --quiet -O ../cli/zkeys/TallyVotes_10-1-2.test \
     https://macitestcircuits.blob.core.windows.net/test/TallyVotes_10-1-2.test
+
+wget --quiet -O ../cli/zkeys/TallyVotes_10-1-2.test.dat \
+    https://macitestcircuits.blob.core.windows.net/test/TallyVotes_10-1-2.test.dat
+
+chmod a+x ../cli/zkeys/ProcessMessages_10-2-1-2.test
+chmod a+x ../cli/zkeys/TallyVotes_10-1-2.test
 
 cd ../contracts && npm run hardhat &
 sleep 5
