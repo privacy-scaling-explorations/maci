d3a13111895b7d4d56d1a05c89fb51aec8963093
Koh Wei Jie <contact@kohweijie.com>
2021-06-30 15:50:31 +0100

    circuits/scripts/installGlibc.sh

--- circuits/scripts/installGlibc.sh
+++ circuits/scripts/installGlibc.sh
@@ -4,4 +4,4 @@ cd "$(dirname "$0")"
 cd ..
 
 wget http://launchpadlibrarian.net/531361873/libc6_2.33-0ubuntu5_amd64.deb
-sudo dpkg --auto-deconfigure -i libc6_2.33-0ubuntu5_amd64.deb
+sudo dpkg --auto-deconfigure -i libc6_2.33-0ubuntu5_amd64.deb || true
