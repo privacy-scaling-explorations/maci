d6a875b30fa0fd6677bf290d5ec80b7cddfeb8fd
Koh Wei Jie <contact@kohweijie.com>
2021-11-08 22:40:17 +0100

    updated zkey download script

--- integrationTests/scripts/runTestsInCi.sh
+++ integrationTests/scripts/runTestsInCi.sh
@@ -5,26 +5,26 @@ cd ..
 
 mkdir -p ../cli/zkeys
 
-wget --quiet -O ../cli/zkeys/ProcessMessages_10-2-1-2.test.0.zkey \
-    https://macitestcircuits.blob.core.windows.net/test/ProcessMessages_10-2-1-2.test.0.zkey
+wget --quiet -O ../cli/zkeys/ProcessMessages_10-2-1-2_test.0.zkey \
+    https://macitestcircuits.blob.core.windows.net/circom2/ProcessMessages_10-2-1-2_test.0.zkey
 
-wget --quiet -O ../cli/zkeys/ProcessMessages_10-2-1-2.test \
-    https://macitestcircuits.blob.core.windows.net/test/ProcessMessages_10-2-1-2.test
+wget --quiet -O ../cli/zkeys/ProcessMessages_10-2-1-2_test \
+    https://macitestcircuits.blob.core.windows.net/circom2/ProcessMessages_10-2-1-2_test
 
-wget --quiet -O ../cli/zkeys/ProcessMessages_10-2-1-2.test.dat \
-    https://macitestcircuits.blob.core.windows.net/test/ProcessMessages_10-2-1-2.test.dat
+wget --quiet -O ../cli/zkeys/ProcessMessages_10-2-1-2_test.dat \
+    https://macitestcircuits.blob.core.windows.net/circom2/ProcessMessages_10-2-1-2_test.dat
 
-wget --quiet -O ../cli/zkeys/TallyVotes_10-1-2.test.0.zkey \
-    https://macitestcircuits.blob.core.windows.net/test/TallyVotes_10-1-2.test.0.zkey
+wget --quiet -O ../cli/zkeys/TallyVotes_10-1-2_test.0.zkey \
+    https://macitestcircuits.blob.core.windows.net/circom2/TallyVotes_10-1-2_test.0.zkey
 
-wget --quiet -O ../cli/zkeys/TallyVotes_10-1-2.test \
-    https://macitestcircuits.blob.core.windows.net/test/TallyVotes_10-1-2.test
+wget --quiet -O ../cli/zkeys/TallyVotes_10-1-2_test \
+    https://macitestcircuits.blob.core.windows.net/circom2/TallyVotes_10-1-2_test
 
-wget --quiet -O ../cli/zkeys/TallyVotes_10-1-2.test.dat \
-    https://macitestcircuits.blob.core.windows.net/test/TallyVotes_10-1-2.test.dat
+wget --quiet -O ../cli/zkeys/TallyVotes_10-1-2_test.dat \
+    https://macitestcircuits.blob.core.windows.net/circom2/TallyVotes_10-1-2_test.dat
 
-chmod a+x ../cli/zkeys/ProcessMessages_10-2-1-2.test
-chmod a+x ../cli/zkeys/TallyVotes_10-1-2.test
+chmod a+x ../cli/zkeys/ProcessMessages_10-2-1-2_test
+chmod a+x ../cli/zkeys/TallyVotes_10-1-2_test
 
 cd ../contracts && npm run hardhat &
 sleep 5
