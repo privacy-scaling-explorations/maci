27c396535cf5d8d20c3ae4bd5807f2416c2e156b
Koh Wei Jie <contact@kohweijie.com>
2021-11-10 16:00:10 +0100

    updated test circuit tar.gz file URL

--- circuits/scripts/downloadTestCircuits.sh
+++ circuits/scripts/downloadTestCircuits.sh
@@ -7,6 +7,6 @@ mkdir -p build/test/
 cd build/test
 
 wget --quiet -O circuits.tar.gz \
-    https://macitestcircuits.blob.core.windows.net/test/big_test_circuits.tar.gz
+    https://macitestcircuits.blob.core.windows.net/circom2/macitestcircuits.tar.gz
 tar xf circuits.tar.gz
 rm circuits.tar.gz
