b899cc804fd81aa360b17f590ec9cb67e6778235
Cory Dickson <cory.dickson@protonmail.com>
2022-02-18 13:49:12 -0500

    Fix mint function

--- contracts/contracts/SignUpToken.sol
+++ contracts/contracts/SignUpToken.sol
@@ -8,7 +8,7 @@ contract SignUpToken is ERC721, Ownable {
     constructor() ERC721("SignUpToken", "SignUpToken") { }
 
     // Gives an ERC721 token to an address
-    function giveToken(address to) public onlyOwner {
+    function giveToken(address to, uint256 curTokenId) public onlyOwner {
         _mint(to, curTokenId);
     }
 }
