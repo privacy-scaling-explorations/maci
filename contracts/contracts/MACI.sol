d62c7c710ba126ced713b8d32190408dbf5fa29f
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2022-09-30 13:24:34 +0000

    fix(maci): amended the `signUp` function to reflect the checks-effects-interaction pattern
    Moved the `_pubKey` check above before increasing the number of signups.
    
    fix #503

--- contracts/contracts/MACI.sol
+++ contracts/contracts/MACI.sol
@@ -199,17 +199,17 @@ contract MACI is IMACI, DomainObjs, Params, SnarkCommon, Ownable {
             "MACI: maximum number of signups reached"
         );
 
+        require(
+            _pubKey.x < SNARK_SCALAR_FIELD && _pubKey.y < SNARK_SCALAR_FIELD,
+            "MACI: _pubKey values should be less than the snark scalar field"
+        );
+
         // Increment the number of signups
         numSignUps++;
 
         // Register the user via the sign-up gatekeeper. This function should
         // throw if the user has already registered or if ineligible to do so.
         signUpGatekeeper.register(msg.sender, _signUpGatekeeperData);
-
-        require(
-            _pubKey.x < SNARK_SCALAR_FIELD && _pubKey.y < SNARK_SCALAR_FIELD,
-            "MACI: _pubKey values should be less than the snark scalar field"
-        );
 
         // Get the user's voice credit balance.
         uint256 voiceCreditBalance = initialVoiceCreditProxy.getVoiceCredits(
