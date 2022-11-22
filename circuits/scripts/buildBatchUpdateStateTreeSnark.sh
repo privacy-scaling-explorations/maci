88832f7de806588c9abec237b87d7461c06c8a41
Brendan Weinstein <brendan.weinstein@coinbase.com>
2022-11-21 17:18:50 -0700

    fix missing memory limit parameter

--- circuits/scripts/buildBatchUpdateStateTreeSnark.sh
+++ circuits/scripts/buildBatchUpdateStateTreeSnark.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")"
 cd ..
 mkdir -p params
 
-NODE_OPTIONS=--max-old-space-size=8192 node build/buildSnarks.js -i circom/test/batchUpdateStateTree_test.circom -j params/batchUstCircuit.r1cs -c params/batchUst.c -y params/batchUst.sym -p params/batchUstPk.json -v params/batchUstVk.json -s params/BatchUpdateStateTreeVerifier.sol -vs BatchUpdateStateTreeVerifier -pr params/batchUst.params -w params/batchUst -a params/batchUst.wasm
+NODE_OPTIONS=--max-old-space-size=8192 node build/buildSnarks.js -ml 8192 -i circom/test/batchUpdateStateTree_test.circom -j params/batchUstCircuit.r1cs -c params/batchUst.c -y params/batchUst.sym -p params/batchUstPk.json -v params/batchUstVk.json -s params/BatchUpdateStateTreeVerifier.sol -vs BatchUpdateStateTreeVerifier -pr params/batchUst.params -w params/batchUst -a params/batchUst.wasm
 
 echo 'Copying BatchUpdateStateTreeVerifier.sol to contracts/sol.'
 cp ./params/BatchUpdateStateTreeVerifier.sol ../contracts/sol/
