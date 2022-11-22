8b4d1261ad24ab97e0d9ac6a407db440b87466b1
Brendan Weinstein <brendan.weinstein@coinbase.com>
2022-11-21 17:25:40 -0700

    fix all cases where memory_limit -ml param is missing

--- circuits/scripts/buildSnarksMedium.sh
+++ circuits/scripts/buildSnarksMedium.sh
@@ -7,11 +7,11 @@ cd ..
 mkdir -p params
 
 echo "Building batchUpdateStateTree_medium"
-NODE_OPTIONS=--max-old-space-size=16384 node --stack-size=1073741 build/buildSnarks.js -i circom/prod/batchUpdateStateTree_medium.circom -j params/batchUstMedium.r1cs -c params/batchUstMedium.c -y params/batchUstMedium.sym -p params/batchUstPkMedium.json -v params/batchUstVkMedium.json -s params/BatchUpdateStateTreeVerifierMedium.sol -vs BatchUpdateStateTreeVerifierMedium -pr params/batchUstMedium.params -w params/batchUstMedium -a params/batchUstMedium.wasm
+NODE_OPTIONS=--max-old-space-size=16384 node --stack-size=1073741 build/buildSnarks.js -ml=16384 -i circom/prod/batchUpdateStateTree_medium.circom -j params/batchUstMedium.r1cs -c params/batchUstMedium.c -y params/batchUstMedium.sym -p params/batchUstPkMedium.json -v params/batchUstVkMedium.json -s params/BatchUpdateStateTreeVerifierMedium.sol -vs BatchUpdateStateTreeVerifierMedium -pr params/batchUstMedium.params -w params/batchUstMedium -a params/batchUstMedium.wasm
 
 
 echo "Building quadVoteTally_medium"
-NODE_OPTIONS=--max-old-space-size=16384 node --stack-size=1073741 build/buildSnarks.js -i circom/prod/quadVoteTally_medium.circom -j params/qvtCircuitMedium.r1cs -c params/qvtMedium.c -y params/qvtMedium.sym -p params/qvtPkMedium.bin -v params/qvtVkMedium.json -s params/QuadVoteTallyVerifierMedium.sol -vs QuadVoteTallyVerifierMedium -pr params/qvtMedium.params -w params/qvtMedium -a params/qvtMedium.wasm
+NODE_OPTIONS=--max-old-space-size=16384 node --stack-size=1073741 build/buildSnarks.js -ml=16384 -i circom/prod/quadVoteTally_medium.circom -j params/qvtCircuitMedium.r1cs -c params/qvtMedium.c -y params/qvtMedium.sym -p params/qvtPkMedium.bin -v params/qvtVkMedium.json -s params/QuadVoteTallyVerifierMedium.sol -vs QuadVoteTallyVerifierMedium -pr params/qvtMedium.params -w params/qvtMedium -a params/qvtMedium.wasm
 
 echo 'Copying BatchUpdateStateTreeVerifierMedium.sol to contracts/sol.'
 cp ./params/BatchUpdateStateTreeVerifierMedium.sol ../contracts/sol/
--- circuits/scripts/buildSnarksSmall.sh
+++ circuits/scripts/buildSnarksSmall.sh
@@ -7,10 +7,10 @@ cd ..
 mkdir -p params
 
 echo "Building batchUpdateStateTree_small"
-NODE_OPTIONS=--max-old-space-size=16384 node --stack-size=1073741 build/buildSnarks.js -i circom/prod/batchUpdateStateTree_small.circom -j params/batchUstSmall.r1cs -c params/batchUstSmall.c -y params/batchUstSmall.sym -p params/batchUstPkSmall.json -v params/batchUstVkSmall.json -s params/BatchUpdateStateTreeVerifierSmall.sol -vs BatchUpdateStateTreeVerifierSmall -pr params/batchUstSmall.params -w params/batchUstSmall -a params/batchUstSmall.wasm
+NODE_OPTIONS=--max-old-space-size=16384 node --stack-size=1073741 build/buildSnarks.js -ml=16384 -i circom/prod/batchUpdateStateTree_small.circom -j params/batchUstSmall.r1cs -c params/batchUstSmall.c -y params/batchUstSmall.sym -p params/batchUstPkSmall.json -v params/batchUstVkSmall.json -s params/BatchUpdateStateTreeVerifierSmall.sol -vs BatchUpdateStateTreeVerifierSmall -pr params/batchUstSmall.params -w params/batchUstSmall -a params/batchUstSmall.wasm
 
 echo "Building quadVoteTally_small"
-NODE_OPTIONS=--max-old-space-size=16384 node --stack-size=1073741 build/buildSnarks.js -i circom/prod/quadVoteTally_small.circom -j params/qvtCircuitSmall.r1cs -c params/qvtSmall.c -y params/qvtSmall.sym -p params/qvtPkSmall.bin -v params/qvtVkSmall.json -s params/QuadVoteTallyVerifierSmall.sol -vs QuadVoteTallyVerifierSmall -pr params/qvtSmall.params -w params/qvtSmall -a params/qvtSmall.wasm
+NODE_OPTIONS=--max-old-space-size=16384 node --stack-size=1073741 build/buildSnarks.js -ml=16384 -i circom/prod/quadVoteTally_small.circom -j params/qvtCircuitSmall.r1cs -c params/qvtSmall.c -y params/qvtSmall.sym -p params/qvtPkSmall.bin -v params/qvtVkSmall.json -s params/QuadVoteTallyVerifierSmall.sol -vs QuadVoteTallyVerifierSmall -pr params/qvtSmall.params -w params/qvtSmall -a params/qvtSmall.wasm
 
 echo 'Copying BatchUpdateStateTreeVerifierSmall.sol to contracts/sol.'
 cp ./params/BatchUpdateStateTreeVerifierSmall.sol ../contracts/sol/
