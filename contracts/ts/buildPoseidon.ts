0ce643a18704c1a2d9245e8e85032874a11f004e
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2022-11-24 16:24:15 +0000

    fix(maci): upgraded snarkjs and other dependencies

--- contracts/ts/buildPoseidon.ts
+++ contracts/ts/buildPoseidon.ts
@@ -1,8 +1,8 @@
 const { ethers, overwriteArtifact } = require('hardhat')
-const poseidonGenContract = require('circomlib/src/poseidon_gencontract.js')
+const { poseidonContract } = require('circomlibjs')
 
 const buildPoseidon = async (numInputs: number) => {
-    await overwriteArtifact(`PoseidonT${numInputs + 1}`, poseidonGenContract.createCode(numInputs))
+    await overwriteArtifact(`PoseidonT${numInputs + 1}`, poseidonContract.createCode(numInputs))
 }
 
 const buildPoseidonT3 = () => buildPoseidon(2)
