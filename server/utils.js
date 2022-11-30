ed5915eb524b92299b3a52f2de1000161a8f1a32
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2022-11-30 12:02:01 +0000

    fix(server): expanded regex to include capital case values

--- server/utils.js
+++ server/utils.js
@@ -1,6 +1,6 @@
 const whitelist = (input) => {
     // allow alphanumeric, dots, _ and - to match up to a MACI key or a file name
-    if (input.toString().match((/^[0-9a-z\.\_\-]+$/))) return true 
+    if (input.toString().match((/^[0-9a-zA-Z\.\_\-]+$/))) return true 
     return false 
 }
 
