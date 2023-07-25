a6f3d4526066cefa9cc86bc1260273548edba14f
Jeeiii <giacomo.corrias7@gmail.com>
2023-07-25 18:36:55 +0200

    test: add Poll deploy from MACI contract; improve VK comparison

--- contracts/ts/__tests__/E2E.test.ts
+++ contracts/ts/__tests__/E2E.test.ts
@@ -1,7 +1,7 @@
 import { Contract, Signer } from "ethers";
-import { deployConstantInitialVoiceCreditProxy, deployFreeForAllSignUpGatekeeper, deployMaci, deployTopupCredit, deployVerifier, deployVkRegistry } from "../deploy";
+import { deployConstantInitialVoiceCreditProxy, deployFreeForAllSignUpGatekeeper, deployMaci, deployMessageProcessor, deployTally, deployTopupCredit, deployVerifier, deployVkRegistry } from "../deploy";
 import path = require("path");
-import { VerifyingKey } from "maci-domainobjs";
+import { Keypair, PubKey, VerifyingKey } from "maci-domainobjs";
 import { extractVk } from "maci-circuits"
 import { genProcessVkSig, genTallyVkSig, genDeactivationVkSig } from 'maci-core'
 import { compareVks } from "../utils";
@@ -35,16 +35,25 @@ describe("MACI - E2E", () => {
     let maciContract: Contract
     let stateAQContract: Contract
     let pollFactoryContract: Contract
+    let messageProcessorContract: Contract
+    let tallyContract: Contract
     // Contracts Addresses.
     let poseidonT3ContractAddress: string
     let poseidonT4ContractAddress: string
     let poseidonT5ContractAddress: string
     let poseidonT6ContractAddress: string
+    let pollContractAddress: string
 
     // Costants.
     const initialVoiceCredits = 100
     const signUpDeadline = 1692424915 // The deadline for signing up on MACI expressed as unix timestamp in seconds.
     const deactivationPeriod = 86400 // The length of the deactivation period in ms.
+    const pollDuration = 90 // The Poll duration expressed in seconds.
+    const maxMessages = 25 // The max number of supported messages.
+    const maxVoteOptions = 25 // The max number of supported vote options.
+
+    // Keys.
+    let coordinatorKeyPair: Keypair
 
     // zkeys folder path.
     const zKeysPath = `${__dirname}../../../../cli/zkeys`
@@ -74,6 +83,7 @@ describe("MACI - E2E", () => {
 
     it("should initialize MACI infrastructure", async () => {
         vkRegistryContract = await deployVkRegistry(quiet)
+        await vkRegistryContract.deployTransaction.wait()
 
         // DEBUG.
         if (debug)
@@ -142,6 +152,10 @@ describe("MACI - E2E", () => {
             msgTreeDepth
         )
 
+        expect(compareVks(pmVk, pmVkOnChain)).toBe(true)
+        expect(compareVks(tvVk, tvVkOnChain)).toBe(true)
+        expect(compareVks(pdmVk, pdmVkOnChain)).toBe(true)
+
         // DEBUG.
         if (debug) {
             console.log("pmVkOnChain: ", pmVkOnChain)
@@ -151,9 +165,16 @@ describe("MACI - E2E", () => {
 
         // Create.
         topUpCreditContract = await deployTopupCredit(quiet)
+        await topUpCreditContract.deployTransaction.wait()
+
         constantInitialVoiceCreditProxyContract = await deployConstantInitialVoiceCreditProxy(initialVoiceCredits, quiet)
+        await constantInitialVoiceCreditProxyContract.deployTransaction.wait()
+
         freeForAllSignUpGatekeeperContract = await deployFreeForAllSignUpGatekeeper(quiet)
+        await freeForAllSignUpGatekeeperContract.deployTransaction.wait()
+
         verifierContract = await deployVerifier(quiet)
+        await verifierContract.deployTransaction.wait()
 
         const data =
             await deployMaci(
@@ -166,6 +187,8 @@ describe("MACI - E2E", () => {
                 deactivationPeriod,
                 quiet
             );
+        await data.maciContract.deployTransaction.wait()
+
         maciContract = data.maciContract
         pollFactoryContract = data.pollFactoryContract
         stateAQContract = data.stateAqContract
@@ -187,6 +210,74 @@ describe("MACI - E2E", () => {
             console.log('PoseidonT4Contract: ', poseidonT4ContractAddress)
             console.log('PoseidonT5Contract: ', poseidonT5ContractAddress)
             console.log('PoseidonT6Contract: ', poseidonT6ContractAddress)
+        }
+    })
+
+    it("should deploy Poll", async () => {
+        // Create coordinator keypair.
+        coordinatorKeyPair = new Keypair()
+
+        expect(PubKey.isValidSerializedPubKey(coordinatorKeyPair.pubKey.serialize())).toBe(true)
+
+        // Create.
+        messageProcessorContract = await deployMessageProcessor(
+            verifierContract.address,
+            poseidonT3ContractAddress,
+            poseidonT4ContractAddress,
+            poseidonT5ContractAddress,
+            poseidonT6ContractAddress,
+            quiet
+        )
+        await messageProcessorContract.deployTransaction.wait()
+
+        tallyContract = await deployTally(
+            verifierContract.address,
+            poseidonT3ContractAddress,
+            poseidonT4ContractAddress,
+            poseidonT5ContractAddress,
+            poseidonT6ContractAddress,
+            quiet
+        )
+        await tallyContract.deployTransaction.wait()
+
+        if (debug) {
+            console.log("MessageProcessorContract: ", messageProcessorContract.address)
+            console.log("TallyContract: ", tallyContract.address)
+        }
+
+        // Deploy Poll contract from MACI.
+        const tx = await maciContract.deployPoll(
+            messageProcessorContract.address,
+            pollDuration,
+            {
+                maxMessages,
+                maxVoteOptions
+            },
+            {
+                intStateTreeDepth,
+                messageTreeSubDepth: msgBatchDepth,
+                messageTreeDepth: msgTreeDepth,
+                voteOptionTreeDepth,
+            },
+            coordinatorKeyPair.pubKey.asContractParam(),
+            { gasLimit: 10000000 }
+        )
+        const receipt = await tx.wait()
+
+        // Events.
+        const iface = maciContract.interface
+        const log = iface.parseLog(receipt.logs[receipt.logs.length - 1])
+        const name = log.name
+        
+        // Get data.
+        pollContractAddress = log.args._pollAddr
+
+        expect(name).toBe("DeployPoll")
+        expect(pollId).toBe(Number(log.args._pollId))
+
+        if (debug) {
+            console.log("PollContractAddress: ", pollContractAddress)
+            console.log("PollId: ", pollId)
         }
     })
 })
