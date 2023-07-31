9e991dfc1c15b14b8201cd94abadaf4018774390
kurticognjen <kurticognjen@gmail.com>
2023-07-31 15:46:33 +0200

    Fixed duration for key deactivation CLI tests and removed  isAfterVotingDeadline from pool since mergeForDeactivation happens before voting deadline

--- cli/tests/vanilla/testVotingWithKeyDeactivation.sh
+++ cli/tests/vanilla/testVotingWithKeyDeactivation.sh
@@ -24,10 +24,6 @@ deploy_poll
 $MACI_CLI signup \
     --pubkey macipk.3e7bb2d7f0a1b7e980f1b6f363d1e3b7a12b9ae354c2cd60a9cfa9fd12917391
 
-# signup period
-$MACI_CLI timeTravel \
-    --seconds 100
-
 $MACI_CLI deactivateKey \
     --privkey macisk.fd7aa614ec4a82716ffc219c24fd7e7b52a2b63b5afb17e81c22fe21515539c \
     --state-index 1 \
@@ -35,8 +31,6 @@ $MACI_CLI deactivateKey \
     --salt 0x798D81BE4A9870C079B8DE539496AB95 \
     --poll-id $POLL_ID
 
-$MACI_CLI timeTravel \
-    --seconds 10
 
 # --from-block since MACI deployed
 $MACI_CLI confirmDeactivation \
@@ -45,9 +39,9 @@ $MACI_CLI confirmDeactivation \
     --from-block 0 \
     --batch-size 1 \
 
-# key deactivation happens after original period 86400s from prepare_test.sh has expired 
+# key deactivation happens after original period 60s from prepare_test.sh has expired 
 $MACI_CLI timeTravel \
-    --seconds 90000
+    --seconds 35
 
 # missing triggering of smart contract code to pass batches
 $MACI_CLI completeDeactivation \
@@ -59,10 +53,6 @@ $MACI_CLI completeDeactivation \
     --process-deactivation-witnessgen "$ZKEYS_DIR"/ProcessDeactivationMessages_"$PROCESS_DEACTIVATION_MESSAGES_PARAMS" \
     --process-deactivation-zkey "$ZKEYS_DIR"/ProcessDeactivationMessages_"$PROCESS_DEACTIVATION_MESSAGES_PARAMS".0.zkey \
     --rapidsnark ~/rapidsnark/build/prover \
-
-# TODO: consider using genMaciKeypair instead of hardcoded key pairs
-# TODO: remove comments once generateNewKey.ts implemented
-# TODO: check new-key-generation-zkey dir name
 
 $MACI_CLI generateNewKey \
     --new-pub-key macipk.e57109205d2b33b90db8421727a2d4fb91cdf3a8e050fbd12aa5a9f4045585a2 \
@@ -78,18 +68,20 @@ $MACI_CLI generateNewKey \
     --new-key-generation-zkey "$ZKEYS_DIR"/GenerateKeyFromDeactivated_"$NEW_KEY_GENERATION_PARAMS".0.zkey \
     --rapidsnark ~/rapidsnark/build/prover
 
-# $MACI_CLI publish \
-#     --pubkey macipk.e57109205d2b33b90db8421727a2d4fb91cdf3a8e050fbd12aa5a9f4045585a2 \
-#     --privkey macisk.acd54022725c8cf56dcd392808e3c4d170100d9fba4009ef0e8173ffe17f2e0 \
-#     --state-index 1 \
-#     --vote-option-index 0 \
-#     --new-vote-weight 9 \
-#     --nonce 1 \
-#     --poll-id "$POLL_ID"
+$MACI_CLI publish \
+    --pubkey macipk.e57109205d2b33b90db8421727a2d4fb91cdf3a8e050fbd12aa5a9f4045585a2 \
+    --privkey macisk.acd54022725c8cf56dcd392808e3c4d170100d9fba4009ef0e8173ffe17f2e0 \
+    --state-index 1 \
+    --vote-option-index 0 \
+    --new-vote-weight 9 \
+    --nonce 1 \
+    --poll-id $POLL_ID
     
-# $MACI_CLI timeTravel \
-#     --seconds 90
+$MACI_CLI timeTravel \
+    --seconds 60
 
+# TODO: Currently lines below fail because elgamal processing of votes is not complete
+# Uncomment as soon as that part is finished
 # gen_proofs "$POLL_ID"
 
 # prove_and_verify_on_chain "$POLL_ID"
