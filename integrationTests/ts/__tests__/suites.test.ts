fc1ce44d162052de07e2094e829c8923f0436d52
kurticognjen <kurticognjen@gmail.com>
2023-07-31 14:48:55 +0200

    Add separate suite for elgamal with one copied passing e2e test

--- integrationTests/ts/__tests__/suites.test.ts
+++ integrationTests/ts/__tests__/suites.test.ts
@@ -3,13 +3,26 @@ jest.setTimeout(3000000)
 import {
     loadData,
     executeSuite,
+    executeSuiteElgamal
 } from './suites'
 
-describe('Test suites', () => {
-    const data = loadData('suites.json')
+// describe('Test suites', () => {
+//     const data = loadData('suites.json')
+//     for (const test of data.suites) {
+//         it(test.description, async () => {
+//             const result = await executeSuite(test, expect)
+//             console.log(result)
+
+//             expect(result).toBeTruthy()
+//         })
+//     }
+// })
+
+describe('Test suites Elgamal', () => {
+    const data = loadData('suitesElgamal.json')
     for (const test of data.suites) {
         it(test.description, async () => {
-            const result = await executeSuite(test, expect)
+            const result = await executeSuiteElgamal(test, expect)
             console.log(result)
 
             expect(result).toBeTruthy()
