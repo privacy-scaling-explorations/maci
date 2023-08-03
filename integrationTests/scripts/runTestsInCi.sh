35ad5bd84d4cc0183eff8f292dd154c4fec93c97
kurticognjen <kurticognjen@gmail.com>
2023-08-03 15:25:45 +0200

    Add generate new key to e2e elgamal suite; Update maci state process deact messages and generate new key methods to properly prepare circuit inputs in case multiple users present;

--- integrationTests/scripts/runTestsInCi.sh
+++ integrationTests/scripts/runTestsInCi.sh
@@ -3,31 +3,31 @@
 cd "$(dirname "$0")"
 cd ..
 
-mkdir -p ../cli/zkeys
+# mkdir -p ../cli/zkeys
 
-wget --quiet -O ../cli/zkeys/ProcessMessages_10-2-1-2_test.0.zkey \
-    https://macitestcircuits.blob.core.windows.net/circom2/ProcessMessages_10-2-1-2_test.0.zkey
+# wget --quiet -O ../cli/zkeys/ProcessMessages_10-2-1-2_test.0.zkey \
+#     https://macitestcircuits.blob.core.windows.net/circom2/ProcessMessages_10-2-1-2_test.0.zkey
 
-wget --quiet -O ../cli/zkeys/ProcessMessages_10-2-1-2_test \
-    https://macitestcircuits.blob.core.windows.net/circom2/ProcessMessages_10-2-1-2_test
+# wget --quiet -O ../cli/zkeys/ProcessMessages_10-2-1-2_test \
+#     https://macitestcircuits.blob.core.windows.net/circom2/ProcessMessages_10-2-1-2_test
 
-wget --quiet -O ../cli/zkeys/ProcessMessages_10-2-1-2_test.dat \
-    https://macitestcircuits.blob.core.windows.net/circom2/ProcessMessages_10-2-1-2_test.dat
+# wget --quiet -O ../cli/zkeys/ProcessMessages_10-2-1-2_test.dat \
+#     https://macitestcircuits.blob.core.windows.net/circom2/ProcessMessages_10-2-1-2_test.dat
 
-wget --quiet -O ../cli/zkeys/TallyVotes_10-1-2_test.0.zkey \
-    https://macitestcircuits.blob.core.windows.net/circom2/TallyVotes_10-1-2_test.0.zkey
+# wget --quiet -O ../cli/zkeys/TallyVotes_10-1-2_test.0.zkey \
+#     https://macitestcircuits.blob.core.windows.net/circom2/TallyVotes_10-1-2_test.0.zkey
 
-wget --quiet -O ../cli/zkeys/TallyVotes_10-1-2_test \
-    https://macitestcircuits.blob.core.windows.net/circom2/TallyVotes_10-1-2_test
+# wget --quiet -O ../cli/zkeys/TallyVotes_10-1-2_test \
+#     https://macitestcircuits.blob.core.windows.net/circom2/TallyVotes_10-1-2_test
 
-wget --quiet -O ../cli/zkeys/TallyVotes_10-1-2_test.dat \
-    https://macitestcircuits.blob.core.windows.net/circom2/TallyVotes_10-1-2_test.dat
+# wget --quiet -O ../cli/zkeys/TallyVotes_10-1-2_test.dat \
+#     https://macitestcircuits.blob.core.windows.net/circom2/TallyVotes_10-1-2_test.dat
 
-chmod a+x ../cli/zkeys/ProcessMessages_10-2-1-2_test
-chmod a+x ../cli/zkeys/TallyVotes_10-1-2_test
+# chmod a+x ../cli/zkeys/ProcessMessages_10-2-1-2_test
+# chmod a+x ../cli/zkeys/TallyVotes_10-1-2_test
 
-cd ../contracts && npm run hardhat &
+# cd ../contracts && npm run hardhat &
 sleep 5
-npm run test-cli-genMaciKeypair
-npm run test-cli-genMaciPubkey
+# npm run test-cli-genMaciKeypair
+# npm run test-cli-genMaciPubkey
 npm run test-suites
