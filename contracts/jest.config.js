25c3e354e098f73834d587bc9866c047f47cdbb4
Jeeiii <giacomo.corrias7@gmail.com>
2023-08-07 11:33:08 +0200

    chore: exclude e2e test file from contracts to avoid CI error

--- contracts/jest.config.js
+++ contracts/jest.config.js
@@ -6,6 +6,10 @@ module.exports = {
     testPathIgnorePatterns: [
         "<rootDir>/build/",
         "/node_modules/",
+        /// nb. temporary since the E2E test must be moved outside of `contracts` folder
+        /// or is going to break the CI (no `zkeys` folder).
+        /// @todo remove this line after having moved the E2E.
+        "ts/__tests__/E2E.test.ts"
     ],
     testRegex: 'ts/__tests__/.*\\.test\\.ts$',
     moduleFileExtensions: [
