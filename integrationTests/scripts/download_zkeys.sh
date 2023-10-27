f48318ac1c62f3cc2e40d889d05c20c0e9ee45db
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2023-10-27 09:36:45 +0100

    fix(ci) - make avaiable updated zkeys and updated download script

--- integrationTests/scripts/download_zkeys.sh
+++ integrationTests/scripts/download_zkeys.sh
@@ -5,19 +5,12 @@ cd ..
 
 mkdir -p ../cli/zkeys
 
-PKGS="zkeys_10-2-1-2_glibc-211.tar.gz ProcessMessages_10-2-1-2_test.0.zkey TallyVotes_10-1-2_test.0.zkey SubsidyPerBatch_10-1-2_test.0.zkey"
+URL=https://maci-develop-fra.s3.eu-central-1.amazonaws.com/v1.1.2/maci_keys_10-2-1-2_dev.tar.gz
+DIR_NAME="maci_keys.tar.gz"
+OUT_DIR=../cli/
 
-BASE_URL=https://maci-develop-fra.s3.eu-central-1.amazonaws.com/v1.1.1-aa4ba27/10-2-1-2
-OUT_DIR=../cli/zkeys
+echo "downloading $URL"
+curl $URL -o "$OUT_DIR/$DIR_NAME"
+tar -xvf "$OUT_DIR/$DIR_NAME" -C "$OUT_DIR"
 
-for p in $PKGS
-do
-  url="$BASE_URL/$p"
-  echo "downloading $url"
-  curl $url -o "$OUT_DIR/$p"
-  extension="${p##*.}"
-  if [ "$extension" == "gz" ]
-  then
-    tar -xvf "$OUT_DIR/$p" -C "$OUT_DIR"
-  fi
-done
+
