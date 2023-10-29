c12395e3365b4f916bfd5e50db6ed8f7014fcac4
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2023-10-29 21:30:25 +0000

    fix(cli-refactoring) - fix compilation bugs

--- cli/ts/commands/genKeyPair.ts
+++ cli/ts/commands/genKeyPair.ts
@@ -3,12 +3,15 @@ import {
 } from "maci-domainobjs"
 import { banner } from "../utils/banner"
 import { logGreen, success } from "../utils/theme"
+import { GenKeyPairArgs } from "../utils"
 
 /**
  * Generate a new Maci Key Pair
  * and print it to the screen
  */
-export const genKeyPair = () => {
+export const genKeyPair = ({
+    quiet
+}: GenKeyPairArgs) => {
     if(!quiet) banner()
     const keypair = new Keypair()
 
--- cli/ts/commands/showContracts.ts
+++ cli/ts/commands/showContracts.ts
@@ -2,8 +2,11 @@ import { readFileSync } from "fs"
 import { contractAddressesStore } from "../utils/constants"
 import { banner } from "../utils/banner"
 import { logGreen, info } from "../utils/theme"
+import { ShowContractsArgs } from "../utils"
 
-export const showContracts = () => {
+export const showContracts = ({
+    quiet
+}: ShowContractsArgs) => {
     if(!quiet) banner() 
     const data = JSON.parse(readFileSync(contractAddressesStore, "utf8").toString())
 
