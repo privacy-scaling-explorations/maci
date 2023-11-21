e6908b95a2d64fe2b1e94f7aea2833cfb3788616
Sam Richards <sbrichards@gmail.com>
2023-11-21 17:25:46 +0300

    Add basic intro section

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -23,6 +23,15 @@ export default function Home(): JSX.Element {
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
+        <section className={styles.introduction}>
+          <div className="container">
+            <div>
+              <h1>What is MACI?</h1>
+              <p>Minimal Anti-Collusion Infrastructure (MACI) is a private, on-chain, voting system.</p>
+              <p>MACI is a protocol designed to provide a highly secure e-voting solution. It enables organizations to conduct on-chain voting processes with a significantly reduced risk of cheating, such as bribery or collusion. MACI uses zero-knowledge proofs to implement a receipt-free voting scheme, making it impossible for anyone other than the vote coordinator to verify how a specific user voted. This ensures the correct execution of votes and allows anyone to verify the results. It's particularly beneficial for governance and funding events, where its anti-collusion mechanisms help ensure fair and transparent outcomes.</p>
+            </div>
+          </div>
+        </section>
         <HomepageFeatures />
       </main>
     </Layout>
