1ea5bfde0f8d38f659dd8de1c7b089d9c7aa8f91
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2023-11-24 16:45:52 +0000

    feat(revamp-docs) - allow typedoc content to be hosted as an iframe

---
+++ website/setupTypedoc.ts
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const path = require("path");
+
+// Define the directory where the Typedoc HTML files are located
+const typedocDir = path.join(__dirname, "static/typedoc_output");
+
+// Define a recursive function to find all HTML files in a directory
+function findHtmlFiles(dir, fileList = []) {
+    const files = fs.readdirSync(dir);
+
+    files.forEach((file) => {
+        if (fs.statSync(path.join(dir, file)).isDirectory()) {
+            fileList = findHtmlFiles(path.join(dir, file), fileList);
+        } else if (file.endsWith(".html")) {
+            fileList.push(path.join(dir, file));
+        }
+    });
+
+    return fileList;
+}
+
+// Find all HTML files in the Typedoc directory
+const htmlFiles = findHtmlFiles(typedocDir);
+
+// Go through each HTML file and add the target="_parent" attribute to the external links
+htmlFiles.forEach((file) => {
+    let content = fs.readFileSync(file, "utf8");
+
+    // Add the target="_parent" attribute to the external links
+    content = content.replace(
+        /<a href="http/g,
+        '<a target="_parent" href="http'
+    );
+
+    fs.writeFileSync(file, content);
+});
