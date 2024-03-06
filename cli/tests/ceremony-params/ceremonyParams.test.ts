b78830d921d39952fbbca0fe7529340ee47ca59f
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2024-03-06 10:51:50 +0000

    test(ceremony): update ceremony params tests
    update ceremony params tests to use new artifacts, as well as adding non qv tests

--- cli/tests/ceremony-params/ceremonyParams.test.ts
+++ cli/tests/ceremony-params/ceremonyParams.test.ts
@@ -22,10 +22,10 @@ import { DeployArgs, DeployPollArgs, DeployedContracts, GenProofsArgs, SetVerify
 import {
   coordinatorPrivKey,
   coordinatorPubKey,
-  ceremonyProcessMessageZkeyPath,
+  ceremonyProcessMessagesZkeyPath,
   ceremonyTallyVotesZkeyPath,
   ceremonyProcessMessagesWasmPath,
-  cermeonyProcessMessagesWitnessPath,
+  ceremonyProcessMessagesWitnessPath,
   testProofsDirPath,
   testRapidsnarkPath,
   testTallyFilePath,
@@ -37,11 +37,19 @@ import {
   verifyArgs,
   ceremonyProcessMessagesDatPath,
   ceremonyTallyVotesDatPath,
+  ceremonyProcessMessagesNonQvWitnessPath,
+  ceremonyProcessMessagesNonQvDatPath,
+  ceremonyTallyVotesNonQvWitnessPath,
+  ceremonyTallyVotesNonQvDatPath,
+  ceremonyProcessMessagesNonQvZkeyPath,
+  ceremonyTallyVotesNonQvZkeyPath,
+  ceremonyProcessMessagesNonQvWasmPath,
+  ceremonyTallyVotesNonQvWasmPath,
 } from "../constants";
 import { cleanVanilla, isArm } from "../utils";
 
-describe("stress tests", function test() {
-  const messageTreeDepth = 8;
+describe("Stress tests with ceremony params (6,9,2,3)", function test() {
+  const messageTreeDepth = 9;
   const stateTreeDepth = 6;
   const voteOptionTreeDepth = 3;
   const messageBatchDepth = 2;
@@ -64,8 +72,19 @@ describe("stress tests", function test() {
     messageTreeDepth,
     voteOptionTreeDepth,
     messageBatchDepth,
-    processMessagesZkeyPath: ceremonyProcessMessageZkeyPath,
+    processMessagesZkeyPath: ceremonyProcessMessagesZkeyPath,
     tallyVotesZkeyPath: ceremonyTallyVotesZkeyPath,
+  };
+
+  const verifyingKeysNonQvArgs: Omit<SetVerifyingKeysArgs, "signer"> = {
+    quiet: true,
+    stateTreeDepth,
+    intStateTreeDepth,
+    messageTreeDepth,
+    voteOptionTreeDepth,
+    messageBatchDepth,
+    processMessagesZkeyPath: ceremonyProcessMessagesNonQvZkeyPath,
+    tallyVotesZkeyPath: ceremonyTallyVotesNonQvZkeyPath,
   };
 
   const ceremonyDeployArgs: Omit<DeployArgs, "signer"> = {
@@ -86,12 +105,12 @@ describe("stress tests", function test() {
     outputDir: testProofsDirPath,
     tallyFile: testTallyFilePath,
     tallyZkey: ceremonyTallyVotesZkeyPath,
-    processZkey: ceremonyProcessMessageZkeyPath,
+    processZkey: ceremonyProcessMessagesZkeyPath,
     pollId: 0n,
     useWasm,
     coordinatorPrivKey,
     rapidsnark: testRapidsnarkPath,
-    processWitgen: cermeonyProcessMessagesWitnessPath,
+    processWitgen: ceremonyProcessMessagesWitnessPath,
     tallyWitgen: ceremonyTallyVotesWitnessPath,
     processWasm: ceremonyProcessMessagesWasmPath,
     tallyWasm: ceremonyTallyVotesWasmPath,
@@ -99,62 +118,249 @@ describe("stress tests", function test() {
     tallyDatFile: ceremonyTallyVotesDatPath,
   };
 
-  // before all tests we deploy the vk registry contract and set the verifying keys
-  before(async () => {
-    signer = await getDefaultSigner();
+  const users = Array<Keypair>(100).fill(new Keypair());
 
-    // we deploy the vk registry contract
-    await deployVkRegistryContract({ signer });
-    // we set the verifying keys
-    await setVerifyingKeys({ ...verifyingKeysArgs, signer });
+  describe("quadratic voting", () => {
+    // before all tests we deploy the vk registry contract and set the verifying keys
+    before(async () => {
+      signer = await getDefaultSigner();
+
+      // we deploy the vk registry contract
+      await deployVkRegistryContract({ signer });
+      // we set the verifying keys
+      await setVerifyingKeys({ ...verifyingKeysArgs, signer });
+    });
+
+    describe("1 user, 2 messages", () => {
+      after(() => {
+        cleanVanilla();
+      });
+
+      before(async () => {
+        // deploy the smart contracts
+        maciAddresses = await deploy({ ...ceremonyDeployArgs, signer });
+        // deploy a poll contract
+        await deployPoll({ ...deployPollArgs, signer });
+      });
+
+      it("should signup 1 user", async () => {
+        await signup({ maciAddress: maciAddresses.maciAddress, maciPubKey: users[0].pubKey.serialize(), signer });
+      });
+
+      it("should publish 2 messages", async () => {
+        // eslint-disable-next-line @typescript-eslint/prefer-for-of
+        for (let i = 0; i < 2; i += 1) {
+          const randomVoteOption = BigInt(Math.floor(Math.random() * 126));
+          const randomVoteWeight = BigInt(Math.floor(Math.random() * 10));
+          // eslint-disable-next-line no-await-in-loop
+          await publish({
+            pubkey: users[0].pubKey.serialize(),
+            stateIndex: BigInt(i) + 1n,
+            voteOptionIndex: randomVoteOption,
+            nonce: 1n,
+            newVoteWeight: randomVoteWeight,
+            maciContractAddress: maciAddresses.maciAddress,
+            salt: genRandomSalt(),
+            privateKey: users[0].privKey.serialize(),
+            pollId: 0n,
+            signer,
+          });
+        }
+      });
+
+      it("should generate zk-SNARK proofs and verify them", async () => {
+        await timeTravel({ seconds: pollDuration, signer });
+        await mergeMessages({ ...mergeMessagesArgs, signer });
+        await mergeSignups({ ...mergeSignupsArgs, signer });
+        await genProofs({ ...genProofsCeremonyArgs, signer });
+        await proveOnChain({ ...proveOnChainArgs, signer });
+        await verify({ ...verifyArgs(), signer });
+      });
+    });
+
+    describe("25 signups, 100 messages", () => {
+      after(() => {
+        cleanVanilla();
+      });
+
+      before(async () => {
+        // deploy the smart contracts
+        maciAddresses = await deploy({ ...ceremonyDeployArgs, signer });
+        // deploy a poll contract
+        await deployPoll({ ...deployPollArgs, signer });
+      });
+
+      it("should signup 25 users", async () => {
+        // eslint-disable-next-line @typescript-eslint/prefer-for-of
+        for (let i = 0; i < 25; i += 1) {
+          // eslint-disable-next-line no-await-in-loop
+          await signup({ maciAddress: maciAddresses.maciAddress, maciPubKey: users[i].pubKey.serialize(), signer });
+        }
+      });
+
+      it("should publish 100 messages", async () => {
+        // eslint-disable-next-line @typescript-eslint/prefer-for-of
+        for (let i = 0; i < users.length; i += 1) {
+          const randomVoteOption = BigInt(Math.floor(Math.random() * 126));
+          const randomVoteWeight = BigInt(Math.floor(Math.random() * 10));
+          // eslint-disable-next-line no-await-in-loop
+          await publish({
+            pubkey: users[i].pubKey.serialize(),
+            stateIndex: BigInt(i) + 1n,
+            voteOptionIndex: randomVoteOption,
+            nonce: 1n,
+            pollId: 0n,
+            newVoteWeight: randomVoteWeight,
+            maciContractAddress: maciAddresses.maciAddress,
+            salt: genRandomSalt(),
+            privateKey: users[i].privKey.serialize(),
+            signer,
+          });
+        }
+      });
+
+      it("should generate zk-SNARK proofs and verify them", async () => {
+        await timeTravel({ seconds: pollDuration, signer });
+        await mergeMessages({ ...mergeMessagesArgs, signer });
+        await mergeSignups({ ...mergeSignupsArgs, signer });
+        await genProofs({ ...genProofsCeremonyArgs, signer });
+        await proveOnChain({ ...proveOnChainArgs, signer });
+        await verify({ ...verifyArgs(), signer });
+      });
+    });
   });
 
-  const users = Array<Keypair>(2).fill(new Keypair());
+  describe("non quadratic voting", () => {
+    const genProofsArgs: Omit<GenProofsArgs, "signer"> = {
+      outputDir: testProofsDirPath,
+      tallyFile: testTallyFilePath,
+      tallyZkey: ceremonyTallyVotesNonQvZkeyPath,
+      processZkey: ceremonyProcessMessagesNonQvZkeyPath,
+      pollId: 0n,
+      rapidsnark: testRapidsnarkPath,
+      processWitgen: ceremonyProcessMessagesNonQvWitnessPath,
+      processDatFile: ceremonyProcessMessagesNonQvDatPath,
+      tallyWitgen: ceremonyTallyVotesNonQvWitnessPath,
+      tallyDatFile: ceremonyTallyVotesNonQvDatPath,
+      coordinatorPrivKey,
+      processWasm: ceremonyProcessMessagesNonQvWasmPath,
+      tallyWasm: ceremonyTallyVotesNonQvWasmPath,
+      useWasm,
+      useQuadraticVoting: false,
+    };
 
-  describe("1 user, 2 messages", () => {
-    after(() => {
-      cleanVanilla();
-    });
-
+    // before all tests we deploy the vk registry contract and set the verifying keys
     before(async () => {
-      // deploy the smart contracts
-      maciAddresses = await deploy({ ...ceremonyDeployArgs, signer });
-      // deploy a poll contract
-      await deployPoll({ ...deployPollArgs, signer });
+      signer = await getDefaultSigner();
+
+      // we deploy the vk registry contract
+      await deployVkRegistryContract({ signer });
+      // we set the verifying keys
+      await setVerifyingKeys({ ...verifyingKeysNonQvArgs, signer });
     });
 
-    it("should signup 1 user", async () => {
-      await signup({ maciAddress: maciAddresses.maciAddress, maciPubKey: users[0].pubKey.serialize(), signer });
-    });
+    describe("1 signup, 1 message", () => {
+      after(() => {
+        cleanVanilla();
+      });
 
-    it("should publish 2 messages", async () => {
-      // eslint-disable-next-line @typescript-eslint/prefer-for-of
-      for (let i = 0; i < 2; i += 1) {
-        const randomVoteOption = BigInt(Math.floor(Math.random() * 126));
-        const randomVoteWeight = BigInt(Math.floor(Math.random() * 10));
-        // eslint-disable-next-line no-await-in-loop
+      before(async () => {
+        // deploy the smart contracts
+        maciAddresses = await deploy({ ...ceremonyDeployArgs, signer, useQv: false });
+        // deploy a poll contract
+        await deployPoll({ ...deployPollArgs, signer });
+      });
+
+      it("should signup one user", async () => {
+        await signup({ maciAddress: maciAddresses.maciAddress, maciPubKey: users[0].pubKey.serialize(), signer });
+      });
+
+      it("should publish one message", async () => {
         await publish({
           pubkey: users[0].pubKey.serialize(),
-          stateIndex: BigInt(i) + 1n,
-          voteOptionIndex: randomVoteOption,
+          stateIndex: 1n,
+          voteOptionIndex: 0n,
           nonce: 1n,
-          newVoteWeight: randomVoteWeight,
+          pollId: 0n,
+          newVoteWeight: 9n,
           maciContractAddress: maciAddresses.maciAddress,
           salt: genRandomSalt(),
           privateKey: users[0].privKey.serialize(),
-          pollId: 0n,
           signer,
         });
-      }
+      });
+
+      it("should generate zk-SNARK proofs and verify them", async () => {
+        await timeTravel({ seconds: pollDuration, signer });
+        await mergeMessages({ ...mergeMessagesArgs, signer });
+        await mergeSignups({ ...mergeSignupsArgs, signer });
+        const tallyFileData = await genProofs({ ...genProofsArgs, signer, useQuadraticVoting: false });
+        await proveOnChain({ ...proveOnChainArgs, signer });
+        await verify({
+          ...verifyArgs(),
+          tallyData: tallyFileData,
+          maciAddress: tallyFileData.maci,
+          tallyAddress: tallyFileData.tallyAddress,
+          signer,
+        });
+      });
     });
 
-    it("should generate zk-SNARK proofs and verify them", async () => {
-      await timeTravel({ seconds: pollDuration, signer });
-      await mergeMessages({ ...mergeMessagesArgs, signer });
-      await mergeSignups({ ...mergeSignupsArgs, signer });
-      await genProofs({ ...genProofsCeremonyArgs, signer });
-      await proveOnChain({ ...proveOnChainArgs, signer });
-      await verify({ ...verifyArgs(), signer });
+    describe("25 signups, 100 messages", () => {
+      after(() => {
+        cleanVanilla();
+      });
+
+      before(async () => {
+        // deploy the smart contracts
+        maciAddresses = await deploy({ ...ceremonyDeployArgs, signer, useQv: false });
+        // deploy a poll contract
+        await deployPoll({ ...deployPollArgs, signer });
+      });
+
+      it("should signup 25 users", async () => {
+        // eslint-disable-next-line @typescript-eslint/prefer-for-of
+        for (let i = 0; i < 25; i += 1) {
+          // eslint-disable-next-line no-await-in-loop
+          await signup({ maciAddress: maciAddresses.maciAddress, maciPubKey: users[i].pubKey.serialize(), signer });
+        }
+      });
+
+      it("should publish 100 messages", async () => {
+        // eslint-disable-next-line @typescript-eslint/prefer-for-of
+        for (let i = 0; i < users.length; i += 1) {
+          const randomVoteOption = BigInt(Math.floor(Math.random() * 126));
+          const randomVoteWeight = BigInt(Math.floor(Math.random() * 10));
+          // eslint-disable-next-line no-await-in-loop
+          await publish({
+            pubkey: users[i].pubKey.serialize(),
+            stateIndex: BigInt(i) + 1n,
+            voteOptionIndex: randomVoteOption,
+            nonce: 1n,
+            pollId: 0n,
+            newVoteWeight: randomVoteWeight,
+            maciContractAddress: maciAddresses.maciAddress,
+            salt: genRandomSalt(),
+            privateKey: users[i].privKey.serialize(),
+            signer,
+          });
+        }
+      });
+
+      it("should generate zk-SNARK proofs and verify them", async () => {
+        await timeTravel({ seconds: pollDuration, signer });
+        await mergeMessages({ ...mergeMessagesArgs, signer });
+        await mergeSignups({ ...mergeSignupsArgs, signer });
+        const tallyFileData = await genProofs({ ...genProofsArgs, signer, useQuadraticVoting: false });
+        await proveOnChain({ ...proveOnChainArgs, signer });
+        await verify({
+          ...verifyArgs(),
+          tallyData: tallyFileData,
+          maciAddress: tallyFileData.maci,
+          tallyAddress: tallyFileData.tallyAddress,
+          signer,
+        });
+      });
     });
   });
 });
