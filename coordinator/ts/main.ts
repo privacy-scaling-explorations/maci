71014a967920aada6ca58a1885c684dac4eb12b5
kittybest <wlphg010688@gmail.com>
2024-06-19 19:41:25 +0800

    chore: add port as env variable and remove redundant param in testScript (#1577)

--- coordinator/ts/main.ts
+++ coordinator/ts/main.ts
@@ -39,7 +39,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.COORDINATOR_PORT ?? 3000);
 }
 
 bootstrap();
