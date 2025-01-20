1aa4ea318c0cc5f6fdadc12c59357cf4bb9b177e
Anton <14254374+0xmad@users.noreply.github.com>
2025-01-20 16:18:55 -0600

    feat(relayer): relay messages onchain

--- apps/relayer/ts/messageBatch/messageBatch.service.ts
+++ apps/relayer/ts/messageBatch/messageBatch.service.ts
@@ -1,5 +1,9 @@
 import { Injectable, Logger } from "@nestjs/common";
 import { validate } from "class-validator";
+import flatten from "lodash/flatten";
+import uniqBy from "lodash/uniqBy";
+import { MACI__factory as MACIFactory, Poll__factory as PollFactory } from "maci-contracts";
+import { PubKey } from "maci-domainobjs";
 
 import type { MessageBatchDto } from "./dto";
 
@@ -49,14 +53,40 @@ export class MessageBatchService {
       throw new Error("Validation error");
     }
 
-    const ipfsHash = await this.ipfsService.add(args.map(({ messages }) => messages)).catch((error) => {
+    const allMessages = flatten(args.map((item) => item.messages));
+
+    const ipfsHash = await this.ipfsService.add(allMessages).catch((error) => {
       this.logger.error(`Upload message batches to ipfs error:`, error);
       throw error;
     });
 
-    return this.messageBatchRepository.create(args.map(({ messages }) => ({ messages, ipfsHash }))).catch((error) => {
-      this.logger.error(`Save message batch error:`, error);
-      throw error;
-    });
+    const messageBatches = await this.messageBatchRepository
+      .create(args.map(({ messages }) => ({ messages, ipfsHash })))
+      .catch((error) => {
+        this.logger.error(`Save message batch error:`, error);
+        throw error;
+      });
+
+    const [{ maciAddress, pollId }] = uniqBy(
+      allMessages.map(({ maciContractAddress, poll }) => ({
+        maciAddress: maciContractAddress,
+        pollId: poll,
+      })),
+      "maciContractAddress",
+    );
+
+    const maciContract = MACIFactory.connect(maciAddress);
+    const pollAddresses = await maciContract.polls(pollId);
+    const pollContract = PollFactory.connect(pollAddresses.poll);
+
+    const messageHashes = await Promise.all(
+      allMessages.map(({ data, publicKey }) =>
+        pollContract.hashMessageAndEncPubKey({ data }, PubKey.deserialize(publicKey).asContractParam()),
+      ),
+    );
+
+    await pollContract.relayMessagesBatch(messageHashes, ipfsHash).then((tx) => tx.wait());
+
+    return messageBatches;
   }
 }
