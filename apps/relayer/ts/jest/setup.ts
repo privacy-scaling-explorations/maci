b823c593c29fba9b64db864cb9486e0d841d1836
0xmad <0xmad@users.noreply.github.com>
2025-03-04 14:37:17 -0600

    chore(relayer): disable typescript type import error for jest setup

--- apps/relayer/ts/jest/setup.ts
+++ apps/relayer/ts/jest/setup.ts
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore type import
 import type { HardhatEthersHelpers } from "@nomicfoundation/hardhat-ethers/types";
 import type { ethers } from "ethers";
 
@@ -10,6 +12,7 @@ declare module "hardhat/types/runtime.js" {
     // We omit the ethers field because it is redundant.
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore commonjs and esm imports fix
+    // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
     ethers: typeof ethers & HardhatEthersHelpers;
   }
 }
