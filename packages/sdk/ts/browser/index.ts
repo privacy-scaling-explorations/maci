12ff0aa5c2524a30e04292dd217b199789dabb5e
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2025-03-28 21:30:48 +0100

    fix: add export without deploy functions

---
+++ packages/sdk/ts/browser/index.ts
@@ -0,0 +1,37 @@
+export * from "../verifyingKeys";
+export * from "../maci";
+export * from "../relayer";
+export * from "../poll";
+export * from "../proof";
+export * from "../tally";
+export * from "../trees";
+export * from "../vote";
+export * from "../utils";
+export * from "../user";
+export * from "../maciKeys";
+export {
+  EMode,
+  EContracts,
+  EGatekeepers,
+  EInitialVoiceCreditProxies,
+  extractVk,
+  genProofSnarkjs,
+  formatProofForVerifierContract,
+  verifyProof,
+  cleanThreads,
+  unlinkFile,
+  getBlockTimestamp,
+  genEmptyBallotRoots,
+} from "maci-contracts";
+
+export type {
+  FullProveResult,
+  IDeployParams,
+  IMergeParams,
+  IProveParams,
+  IVerifyingKeyStruct,
+  SnarkProof,
+  IIpfsMessage,
+} from "maci-contracts";
+
+export * from "maci-contracts/typechain-types";
