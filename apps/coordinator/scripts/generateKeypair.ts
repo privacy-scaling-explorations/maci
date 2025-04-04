1a9718d2ace25f2f67471685027e63f1950b3ab7
Dmitry <98899785+mdqst@users.noreply.github.com>
2025-04-04 09:28:21 +0300

    fix: dotenv config and improve error handling in key generation

--- apps/coordinator/scripts/generateKeypair.ts
+++ apps/coordinator/scripts/generateKeypair.ts
@@ -1,5 +1,4 @@
 import dotenv from "dotenv";
-
 import { generateKeyPairSync } from "crypto";
 import fs from "fs";
 import path from "path";
@@ -11,22 +10,30 @@ const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 /* eslint-enable no-underscore-dangle */
 /* eslint-enable @typescript-eslint/no-shadow */
 
-dotenv.config({ path: [path.resolve(__dirname, "../.env"), path.resolve(__dirname, "../.env.example")] });
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 const MODULUS_LENGTH = 4096;
 
 export async function generateRsaKeypair(): Promise<void> {
-  const keypair = generateKeyPairSync("rsa", {
-    modulusLength: MODULUS_LENGTH,
-  });
+  const publicKeyPath = process.env.COORDINATOR_PUBLIC_KEY_PATH;
+  const privateKeyPath = process.env.COORDINATOR_PRIVATE_KEY_PATH;
 
-  const publicKey = keypair.publicKey.export({ type: "pkcs1", format: "pem" });
-  const privateKey = keypair.privateKey.export({ type: "pkcs1", format: "pem" });
+  if (!publicKeyPath || !privateKeyPath) {
+    throw new Error("Public or private key path is not set in environment variables.");
+  }
 
-  await Promise.all([
-    fs.promises.writeFile(path.resolve(process.env.COORDINATOR_PUBLIC_KEY_PATH!), publicKey),
-    fs.promises.writeFile(path.resolve(process.env.COORDINATOR_PRIVATE_KEY_PATH!), privateKey),
-  ]);
+  try {
+    const keypair = generateKeyPairSync("rsa", { modulusLength: MODULUS_LENGTH });
+    const publicKey = keypair.publicKey.export({ type: "pkcs1", format: "pem" });
+    const privateKey = keypair.privateKey.export({ type: "pkcs1", format: "pem" });
+
+    await Promise.all([
+      fs.promises.writeFile(publicKeyPath, publicKey),
+      fs.promises.writeFile(privateKeyPath, privateKey),
+    ]);
+  } catch (error) {
+    console.error("Error generating RSA keypair:", error);
+  }
 }
 
 generateRsaKeypair();
