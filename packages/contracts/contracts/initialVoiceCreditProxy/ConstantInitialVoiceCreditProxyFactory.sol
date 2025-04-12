8ce4c0a0c176cd90b8c523ba64f7fbcc4a256c8c
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2025-04-12 18:25:24 +0100

    feat(contracts): return voice credit proxy clone address on deploy (#2358)

--- packages/contracts/contracts/initialVoiceCreditProxy/ConstantInitialVoiceCreditProxyFactory.sol
+++ packages/contracts/contracts/initialVoiceCreditProxy/ConstantInitialVoiceCreditProxyFactory.sol
@@ -14,9 +14,10 @@ contract ConstantInitialVoiceCreditProxyFactory is Factory {
 
   /// @notice Deploys a new ConstantInitialVoiceCreditProxy clone.
   /// @param balance the balance to be returned by getVoiceCredits
-  function deploy(uint256 balance) public {
+  /// @return clone the address of the new clone
+  function deploy(uint256 balance) public returns (address clone) {
     bytes memory data = abi.encode(balance);
-    address clone = super._deploy(data);
+    clone = super._deploy(data);
 
     ConstantInitialVoiceCreditProxy(clone).initialize();
   }
