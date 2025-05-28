85ab6744925e859ca46e24a9f5d9849d639e93cf
GarmashAlex <garmasholeksii@gmail.com>
2025-05-28 18:05:31 +0300

    Update index.ts

--- packages/sdk/ts/user/index.ts
+++ packages/sdk/ts/user/index.ts
@@ -25,6 +25,6 @@ export type {
   IParseSignupEventsArgs,
   ISignupData,
   IHasUserSignedUpArgs,
-  IGenMaciStateTreeAddressArgs,
-  IGenMaciStateTreeWithEndKeyAddressArgs,
+  IGenerateMaciStateTreeAddressArgs,
+  IGenerateMaciStateTreeWithEndKeyAddressArgs,
 } from "./types";
