d5d2ff4e678f4fddf6000b8341cd77259587ad1b
crStiv <cryptostiv7@gmail.com>
2025-07-05 12:53:43 +0200

    Update types.ts

--- packages/sdk/ts/verifyingKeys/types.ts
+++ packages/sdk/ts/verifyingKeys/types.ts
@@ -129,14 +129,14 @@ export interface ISetVerifyingKeysArgs {
   pollJoinedVerifyingKey: VerifyingKey;
 
   /**
-   * The process messages verifying key object
+   * The process messages verifying key objects
    */
-  processMessagesVerifyingKey: VerifyingKey;
+  processMessagesVerifyingKeys: VerifyingKey[];
 
   /**
-   * The tally votes verifying key object
+   * The tally votes verifying key objects
    */
-  tallyVotesVerifyingKey: VerifyingKey;
+  tallyVotesVerifyingKeys: VerifyingKey[];
 
   /**
    * The state tree depth
@@ -174,9 +174,9 @@ export interface ISetVerifyingKeysArgs {
   signer: Signer;
 
   /**
-   * QV or NON_QV
+   * QV, NON_QV, or FULL modes
    */
-  mode: EMode;
+  modes: EMode[];
 }
 
 /**
